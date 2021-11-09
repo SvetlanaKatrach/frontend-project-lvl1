6e3433a376729a9020b00a94bf41f0d05b717b2b
Svetlana Katrach <ssvetik@land.ru>
2021-11-09 12:14:44 +0400

    Update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,31 +1,17 @@
 import readlineSync from 'readline-sync';
-import { getRandomNum, getUserName, greetingUser } from './functions.js';
+import { getRandomNum, isEven, startGame } from './index.js';
 
-console.log(greetingUser());
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minValueOfRandomNum = 1;
+const maxValueOfRandomNum = 100;
 
-const isEven = (num) => num % 2 === 0;
+const getGameResult = () => {
+  const randomNum = getRandomNum(minValueOfRandomNum, maxValueOfRandomNum);
+  const expectedAnswer = isEven(randomNum) ? 'yes' : 'no';
+  const userAnswer = readlineSync.question(`${gameRule} \nQuestion: ${randomNum} \nYour answer: `);
+  return [expectedAnswer, userAnswer];
+};
 
-let counter = 0;
-while (counter < 3) {
-  const randomNumUpTo100 = getRandomNum(1, 100);
-  const userAnswer = readlineSync.question(`Question: ${randomNumUpTo100} \nYour answer: `);
-  const expectedAnswerEven = isEven(randomNumUpTo100) ? 'yes' : 'no';
-  const rightAnswer = 'Correct!';
-  const wrongAnswer = `'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswerEven}'. \nLet's try again, ${getUserName}!`;
+const evenGame = () => startGame(gameRule, getGameResult);
 
-  if (userAnswer === expectedAnswerEven) {
-    console.log(rightAnswer);
-    counter += 1;
-  } else if (userAnswer !== expectedAnswerEven) {
-    console.log(wrongAnswer);
-    counter = 0;
-    break;
-  }
-}
-
-if (counter === 3) {
-  console.log(`Congratulations, ${getUserName}!`);
-}
-
-export default isEven;
+export default evenGame;
