891955845479328ca48209b618bc5446045fbee7
Svetlana Katrach <ssvetik@land.ru>
2021-11-15 12:35:54 +0400

    Update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userName = () => {
   const question = readlineSync.question('May I have your name?');
-  return (`Hello,${question}!`);
+  return (`Hello, ${question}!`);
 };
 
 export default userName;
