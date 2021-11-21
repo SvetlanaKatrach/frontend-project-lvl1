14bc09547a80ad6385816d79bb2a297a3e0f22da
Svetlana Katrach <ssvetik@land.ru>
2021-11-21 12:38:26 +0400

    Refactoring src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
-import { getRandomNum, startGame } from './index.js';
+import { startGame } from '../index.js';
+import { getRandomNum } from '../get-random.js';
 
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
@@ -16,10 +16,9 @@ const isPrime = (randomNum) => {
 };
 
 const getGameResult = () => {
-  const randomNum = getRandomNum(minRandomNum, maxRandomNum);
-  const expectedAnswer = isPrime(randomNum) ? 'yes' : 'no';
-  const userAnswer = readlineSync.question(`Question: ${randomNum} \nYour answer: `);
-  return [expectedAnswer, userAnswer];
+  const expressionInQuestion = getRandomNum(minRandomNum, maxRandomNum);
+  const expectedAnswer = isPrime(expressionInQuestion) ? 'yes' : 'no';
+  return [expectedAnswer, expressionInQuestion];
 };
 
 const primeGame = () => startGame(gameRule, getGameResult);
