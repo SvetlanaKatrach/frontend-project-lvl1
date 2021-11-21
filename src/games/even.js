6c302e9f0240e8844daea41fce300336b2175b37
Svetlana Katrach <ssvetik@land.ru>
2021-11-21 12:36:49 +0400

    Refactoring src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,15 @@
-import readlineSync from 'readline-sync';
-import { getRandomNum, isEven, startGame } from './index.js';
+import { startGame } from '../index.js';
+import { getRandomNum } from '../get-random.js';
 
+const isEven = (num) => num % 2 === 0;
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const minValueOfRandomNum = 1;
 const maxValueOfRandomNum = 100;
 
 const getGameResult = () => {
-  const randomNum = getRandomNum(minValueOfRandomNum, maxValueOfRandomNum);
-  const expectedAnswer = isEven(randomNum) ? 'yes' : 'no';
-  const userAnswer = readlineSync.question(`Question: ${randomNum} \nYour answer: `);
-  return [expectedAnswer, userAnswer];
+  const expressionInQuestion = getRandomNum(minValueOfRandomNum, maxValueOfRandomNum);
+  const expectedAnswer = isEven(expressionInQuestion) ? 'yes' : 'no';
+  return [expectedAnswer, expressionInQuestion];
 };
 
 const evenGame = () => startGame(gameRule, getGameResult);
