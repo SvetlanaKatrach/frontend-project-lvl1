3859e18b5579ddf731e296ea21fa9f02fe4ff692
Svetlana Katrach <ssvetik@land.ru>
2021-11-21 12:56:57 +0400

    Update src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,5 +1,5 @@
-import { startGame } from '../index.js';
-import { getRandomNum } from '../get-random.js';
+import startGame from '../index.js';
+import getRandomNum from '../getRandom.js';
 
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
