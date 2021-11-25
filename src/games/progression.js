fac5f1e6876a80f031006ee348e29bd1c0288209
Svetlana Katrach <ssvetik@land.ru>
2021-11-25 21:45:29 +0400

    Refactoring src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,4 +1,4 @@
-import { startGame } from '../index.js';
+import startGame from '../index.js';
 import getRandomNum from '../getRandom.js';
 
 const gameRule = 'What number is missing in the progression?';
