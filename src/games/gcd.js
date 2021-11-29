2b0c4f87903af0bd619060e5dd8c4d17192b4076
Svetlana Katrach <ssvetik@land.ru>
2021-11-29 21:44:25 +0400

    Refactoring src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,10 +3,8 @@ import getRandomNum from '../getRandom.js';
 
 const gameRule = 'Find the greatest common divisor of given numbers.';
 
-const getCommonDivisor = (number1, number2) => {
-  const result = (number2 === 0) ? number1 : (getCommonDivisor(number2, number1 % number2));
-  return result;
-};
+// eslint-disable-next-line max-len
+const getCommonDivisor = (number1, number2) => ((number2 === 0) ? number1 : (getCommonDivisor(number2, number1 % number2)));
 
 const getGameResult = () => {
   const number1 = getRandomNum(2, 30);
